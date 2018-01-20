6bb240e519747714571a332d431131f37476a040
Chandrakanth_c <chandrakanthcandikattu@gmail.com>
2018-01-20 17:07:18 +0530

    MethodOverriding - added

---
+++ src/com/chandrakanth/miscellaneous/MethodOverriddenClass.java
@@ -0,0 +1,24 @@
+package com.chandrakanth.miscellaneous;
+
+import java.io.FileNotFoundException;
+
+import org.apache.log4j.Logger;
+
+public class MethodOverriddenClass {
+
+	private static final Logger logger = Logger.getLogger(MethodOverriddenClass.class);
+
+	private static final String CONSTANT_FIELD = "Juminfu";
+
+	private static MethodOverriddenClass mOR = new MethodOverriddenClass();
+
+	public static void main(String[] argz) throws Exception{
+//		logger.info(mOR.bing());
+	}
+
+	// if this method is marked final, then the subclass cannot override this method
+	public /*final*/ /*static*/ String bing() throws FileNotFoundException{
+		return CONSTANT_FIELD;
+	}
+
+}
---
+++ src/com/chandrakanth/miscellaneous/MethodOverridingClass.java
@@ -0,0 +1,34 @@
+package com.chandrakanth.miscellaneous;
+
+public class MethodOverridingClass extends MethodOverriddenClass {
+
+	private static final MethodOverridingClass mORDNG = new MethodOverridingClass();
+
+	public static void main(String argz[]) {
+
+	}
+
+	// final Method cannot be overridden - if MethodOverriddenClass.bing() is marked final
+	/*public final String bing() {
+		
+	}*/
+
+	
+	/* This is called inherited method overloading - in case if @Override is not 
+	 * specified on this method and method signature is not @Override
+	 * equivalent. 
+	 * If @Override is specified, then method signature 
+	 *  - cannot reduce the visibility in overriding method
+	 *  - cannot override when actual method is static
+	 *  - cannot throw super - Exceptions, if exception is thrown in actual method
+	 *  - cannot throw an exception - if actual method doesn't throw exception at all
+	 *  - can be left without throwing exception  -if actual method is throwing exception
+	 *  - cannot have different return type when compared to actual method
+	 *  - cannot have void return type , when actual method is returning the value
+	*/
+	@Override
+	public String bing() {
+		return "2" ;
+	}
+	
+}
