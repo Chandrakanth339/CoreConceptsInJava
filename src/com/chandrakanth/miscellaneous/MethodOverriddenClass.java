cdbb33e211647974be7e777bb946b1b5f4341f72
Chandrakanth_c <chandrakanthcandikattu@gmail.com>
2018-01-21 07:29:55 +0530

    Method Overriding - Points added

--- src/com/chandrakanth/miscellaneous/MethodOverriddenClass.java
+++ src/com/chandrakanth/miscellaneous/MethodOverriddenClass.java
@@ -1,6 +1,6 @@
 package com.chandrakanth.miscellaneous;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import org.apache.log4j.Logger;
 
@@ -8,8 +8,11 @@ public class MethodOverriddenClass {
 
 	private static final Logger logger = Logger.getLogger(MethodOverriddenClass.class);
 
-	private static final String CONSTANT_FIELD = "Juminfu";
+	private static final String CONSTANT_FIELD = "PARENT";
 
+	
+	
+	
 	private static MethodOverriddenClass mOR = new MethodOverriddenClass();
 
 	public static void main(String[] argz) throws Exception{
@@ -17,8 +20,14 @@ public class MethodOverriddenClass {
 	}
 
 	// if this method is marked final, then the subclass cannot override this method
-	public /*final*/ String bing() throws FileNotFoundException{
+	public /*final*/ String bing() throws IOException{
 		return CONSTANT_FIELD;
 	}
 
+	public static String returnStaticValue() { 
+		return CONSTANT_FIELD;
+	}
+	
+	
+	
 }
--- src/com/chandrakanth/miscellaneous/MethodOverridingClass.java
+++ src/com/chandrakanth/miscellaneous/MethodOverridingClass.java
@@ -1,13 +1,21 @@
 package com.chandrakanth.miscellaneous;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MethodOverridingClass extends MethodOverriddenClass {
 
 	private static final MethodOverridingClass mORDNG = new MethodOverridingClass();
 
-	public static void main(String argz[]) {
-
+	public static void main(String argz[]) throws IOException {
+//		System.out.println(mORDNG.returnStaticValue());
+		MethodOverriddenClass mOVRDN = new MethodOverridingClass();
+		System.out.println(mOVRDN.returnStaticValue());
+		System.out.println(MethodOverriddenClass.returnStaticValue());
+		System.out.println(MethodOverridingClass.returnStaticValue());
+		MethodOverriddenClass mOVC = new MethodOverridingClass();
+		System.out.println(mOVC.bing());
+	
 	}
 
 	// final Method cannot be overridden - if MethodOverriddenClass.bing() is marked final
@@ -15,13 +23,15 @@ public class MethodOverridingClass extends MethodOverriddenClass {
 		
 	}*/
 
-	
 	/* This is called inherited method overloading - in case if @Override is not 
 	 * specified on this method and method signature is not @Override
 	 * equivalent. 
+	 * @Overriding is otherwise called as runtime polymorphism
 	 * If @Override is specified, then method signature 
 	 *  - cannot reduce the visibility in overriding method
 	 *  - cannot override when actual method is static
+	 *  - cannot override when actual method is final
+	 *  - can have covariant return type
 	 *  - cannot throw super - Exceptions, if exception is thrown in actual method
 	 *  - cannot throw an exception - if actual method doesn't throw exception at all
 	 *  - can be left without throwing exception  -if actual method is throwing exception
@@ -30,7 +40,13 @@ public class MethodOverridingClass extends MethodOverriddenClass {
 	*/
 	@Override
 	public String bing() throws FileNotFoundException{
-		return "2" ;
+		return "CHILD" ;
 	}
+	
+	
+	public static String returnStaticValue() { 
+		return "abc";
+	}
+	
 	
 }
