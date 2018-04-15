a205d8f8ee2f0da4818b049cfad50958ea2e6287
Chandrakanth_c <chandrakanthcandikattu@gmail.com>
2018-04-15 18:55:36 +0530

    ++Default, Static methods in Interface

--- src/com/chandrakanth/java8/FunctionalInterfaceSample.java
+++ src/com/chandrakanth/java8/FunctionalInterfaceSample.java
@@ -5,4 +5,12 @@ public interface FunctionalInterfaceSample {
 
 	void singleAbstractMethod(String value);
 
+	default String returnDefaultMethodObject(String object) {
+		return object;
+	}
+	
+	static String returnStaticMethodObject(String object) {
+		return object;
+	}
+	
 }
--- src/com/chandrakanth/java8/Java8SamplesImplementor.java
+++ src/com/chandrakanth/java8/Java8SamplesImplementor.java
@@ -3,10 +3,17 @@ package com.chandrakanth.java8;
 public class Java8SamplesImplementor {
 
 	public static void main(String[] argz) {
-		// Functional Interface having argument - Define the argument with type
+		/* Functional Interface having argument - 
+		 * Define the argument followed by Lambda arrow head followed by block of code*/ 
 		FunctionalInterfaceSample sample = (/*String */s) -> {
 			System.out.println(s);
 		};
+		// call the actual method 
 		sample.singleAbstractMethod("argument value");
+		
+		// static method call defined in Interface 
+		System.out.println(FunctionalInterfaceSample.returnStaticMethodObject("Static method arg"));
+		// default method call defined in interface
+		System.out.println(sample.returnDefaultMethodObject("Default method arg"));
 	}
 }
