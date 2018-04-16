0b8ec3cef5c3ec7b84392b1b867b427d46f2334f
Chandrakanth_c <chandrakanthcandikattu@gmail.com>
2018-04-16 13:04:59 +0530

    ++LocalTime and LocalDateTime samples

--- src/com/chandrakanth/java8/DateTime8.java
+++ src/com/chandrakanth/java8/DateTime8.java
@@ -2,59 +2,79 @@ package com.chandrakanth.java8;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Year;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 
-public class DateTime8 {
+public interface DateTime8 {
 
-	public DateTime8() {
-		super();
-	}
-
-	protected static final LocalDate returnCurrentDate() {
+	public static LocalDate returnCurrentDate() {
 		LocalDate localDate = null;
 		try {
-		// LocalDate.now(); // Returns the date in the format yyyy-MM-dd
-		// LocalDate.of(2031, Month.JANUARY, 28);
-		localDate = LocalDate.of(2014, 2, 26); // 1<={month}<=12 {dayOfMonth} <=31
-		}
-		catch(Exception e) {
+			// LocalDate.now(); // Returns the date in the format yyyy-MM-dd
+			// LocalDate.of(2031, Month.JANUARY, 28);
+			localDate = LocalDate.of(2014, 2, 26); // 1<={month}<=12 {dayOfMonth} <=31
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return localDate;
 	}
-	
+
 	/**
 	 * Method to check if current date's Year is a leap year
+	 * 
 	 * @return
 	 */
-	protected static final boolean returnIsLeapYear() {
+	static boolean returnIsLeapYear() {
 		return returnCurrentDate().isLeapYear();
 	}
-	
-	protected static final LocalDate returnMaxDate() {
+
+	static LocalDate returnMaxDate() {
 		return LocalDate.MAX;
 	}
-	
-	protected static final LocalDate returnMinDate() {
+
+	static LocalDate returnMinDate() {
 		return LocalDate.MIN;
 	}
-	
-	protected static final LocalDate returnDecadeAddedDate() {
+
+	static LocalDate returnDecadeAddedDate() {
 		return returnCurrentDate().minus(1, ChronoUnit.ERAS);
 	}
-	
-	protected static final LocalDate returnParsedDate() {
+
+	static LocalDate returnParsedDate() {
 		return LocalDate.parse("2016-10-01");
 	}
-	
-	protected static final LocalDate returnFirstDayOfMonth() {
+
+	static LocalDate returnFirstDayOfMonth() {
 		return returnCurrentDate().with(TemporalAdjusters.firstDayOfMonth());
 	}
-	
-	protected static final DayOfWeek returnDayOfWeek() {
+
+	static DayOfWeek returnDayOfWeek() {
 		return returnCurrentDate().getDayOfWeek();
 	}
-	
-	
+
+	static boolean returnWhetherDate1B4Date2() {
+		return returnCurrentDate().isBefore(returnCurrentDate().plus(3, ChronoUnit.DAYS));
+	}
+
+	static LocalTime returnCurrentLocalTime() {
+		return
+		// LocalTime.now();
+		LocalTime.of(15, 34, 43, 333);
+	}
+
+	static LocalTime returnParsedTime() {
+		return LocalTime.parse("06:45:07");
+	}
+
+	static LocalDateTime returnCurrentTimeStamp() {
+		return LocalDateTime.now();
+	}
+
+	static LocalDateTime returnParsedTimeStamp() {
+		return LocalDateTime.parse("2015-05-14T06:23");
+	}
+
 }
--- src/com/chandrakanth/java8/Java8SamplesImplementor.java
+++ src/com/chandrakanth/java8/Java8SamplesImplementor.java
@@ -2,7 +2,10 @@ package com.chandrakanth.java8;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Month;
+import java.time.temporal.ChronoUnit;
 
 public class Java8SamplesImplementor {
 
@@ -22,39 +25,78 @@ public class Java8SamplesImplementor {
 		// default method call defined in interface
 		System.out.println(sample.returnDefaultMethodObject("Default method arg"));
 
+		// Java8SamplesImplementor.localDateSamples();
+		Java8SamplesImplementor.localTimeSamples();
+		Java8SamplesImplementor.localDateTimeSamples();
+	}
+
+	public static void localDateSamples() {
 		LocalDate localDate1 = null;
 		LocalDate localDate2 = null;
 		localDate1 = DateTime8.returnCurrentDate();
 		System.out.println(localDate1);
 
-		boolean isLeapYear = DateTime8.returnIsLeapYear();
-		System.out.println(isLeapYear);
+		boolean booleanValue = DateTime8.returnIsLeapYear();
+		System.out.println(booleanValue);
 
-		localDate1 = DateTime8.returnMaxDate(); // Max Date 
+		localDate1 = DateTime8.returnMaxDate(); // Max Date
 		localDate2 = DateTime8.returnMinDate(); // Min Date
 
 		System.out.println(localDate1);
 		System.out.println(localDate2);
 
-		/*Date modified by adding/removing ChronoUnit. gives 
-		Gives negative result in case if the datetime exceeds the max value
-		+999999999-12-31*/
+		/*
+		 * Date modified by adding/removing ChronoUnit. gives Gives negative result in
+		 * case if the datetime exceeds the max value +999999999-12-31
+		 */
 		localDate1 = DateTime8.returnDecadeAddedDate();
-		System.out.println("Modified Date\nby adding century:\t"+localDate1);
-		
+		System.out.println("Modified Date\nby adding century:\t" + localDate1);
+
 		localDate1 = DateTime8.returnParsedDate();
-		System.out.println("Parsed Date:\t\t"+localDate1);
-		
+		System.out.println("Parsed Date:\t\t" + localDate1);
+
 		localDate2 = DateTime8.returnFirstDayOfMonth();
-		System.out.println("First Day Of Month:\t"+localDate2);
-		
+		System.out.println("First Day Of Month:\t" + localDate2);
+
 		DayOfWeek dayOfWeek = DateTime8.returnDayOfWeek();
-		System.out.println("Day Of Week:\t\t"+dayOfWeek);
-		
+		System.out.println("Day Of Week:\t\t" + dayOfWeek);
+
 		Month currentMonth = DateTime8.returnCurrentDate().getMonth();
-		System.out.println("Current Month:\t\t"+currentMonth);
-		
-		
-		
+		System.out.println("Current Month:\t\t" + currentMonth);
+
+		booleanValue = DateTime8.returnWhetherDate1B4Date2();
+		System.out.println("Check whether Date1 is before Date2: " + booleanValue);
+
 	}
+
+	public static void localTimeSamples() {
+		LocalTime localTime1 = null;
+		LocalTime localTime2 = null;
+		localTime1 = DateTime8.returnCurrentLocalTime();
+		System.out.println("Current Local Time: "+localTime1);
+
+		localTime2 = DateTime8.returnParsedTime();
+		System.out.println("Current Parsed Time: "+localTime2);
+
+		localTime1 = DateTime8.returnCurrentLocalTime();
+		localTime2 = DateTime8.returnCurrentLocalTime().plus(3,  ChronoUnit.HOURS);
+		System.out.println("Check if Time1 is after time2"+localTime2.isAfter(localTime1));
+	
+	}
+
+	public static void localDateTimeSamples() {
+		LocalDateTime localDateTime1 = null;
+		LocalDateTime localDateTime2 = null;
+		
+		localDateTime1 = DateTime8.returnCurrentTimeStamp();
+		System.out.println("Current Local Timestamp:"+localDateTime1);
+		
+		localDateTime2 = DateTime8.returnParsedTimeStamp();
+		System.out.println("Current parsed Timestamp: "+localDateTime2);
+		
+		localDateTime2 = localDateTime1.plus(4, ChronoUnit.YEARS);
+		System.out.println("Check if Timestamp1\nis after Timestamp2: "+localDateTime2.isAfter(localDateTime1));
+	}
+	
+	
 }
